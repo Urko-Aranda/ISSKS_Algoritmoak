866fe627c3f8d482c126dab14b88a67450620acf
Urko-Aranda <urkoaragom@gmail.com>
2023-11-13 15:57:53 +0100

    Update backupsLab6.sh

--- backupsLab6.sh
+++ backupsLab6.sh
@@ -7,7 +7,4 @@ fecha_actual=$(date +%Y-%m-%d)
 fecha_ayer=$(date -d "yesterday" +%Y-%m-%d)
 
 
-sync -av "urkoaragom@34.136.240.16:/var/tmp/Backups/$fecha_ayer" "/var/tmp/Backups/$fecha_ayer"
-
-
 rsync -av --link-dest="/var/tmp/Backups/$fecha_ayer" /home/urko/UPV3/ISSKS/LAB/LAB5/Segurtasuna/ "urkoaragom@34.136.240.16:/var/tmp/Backups/$fecha_actual"
